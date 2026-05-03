0999d649e13a99d59fb02ed7e2d80570afd94ad7
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-05-04 02:11:28 +0600

    just finished the beginner level problem now it's time to go to sleep

---
+++ Python codes/Chapter-02/05_Problem5.py
@@ -0,0 +1,7 @@
+# Find the average of two numbers entered by the user
+a = int(input("Enter number a:"))
+b = int(input("Enter number b:"))
+ 
+c = (a + b) / 2
+
+print("Average of a and b is: ", c)
---
+++ Python codes/Chapter-02/06_Problem6.py
@@ -0,0 +1,4 @@
+# Finding the square of a number entered by the user
+
+a = int(input("Enter your number: "))
+print("Square of the given number is: ", a**2)
