c6f4e1e90d43c1a3759ec9f668f032fbe2044ee0
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-05-05 23:12:05 +0600

    just solving the problems from crack

---
+++ Python codes/Chapter-03-PS/03_problem3.py
@@ -0,0 +1,5 @@
+# Find the index of the first occurrence of the substring "goo" in the string "Nahid is a good boy".
+
+name  = "Nahid is a good boy"
+
+print(name.find("goo"))
---
+++ Python codes/Chapter-03-PS/04_problem4.py
@@ -0,0 +1,5 @@
+
+name  = "Nahid is a good boy"
+print(name.replace(" ","  "))
+print(name) # Strings are immutable which means that you cannot change them by running functions on them.
+
