3be1798bc06711c582e13c93927ac901b0a10695
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-05-05 23:15:45 +0600

    Finished chapter 3 practice sets

---
+++ Python codes/Chapter-03-PS/05_problems5.py
@@ -0,0 +1,5 @@
+# Write a program to format the following letter using escape sequence characters.
+# letter = "Dear Nahid,This python course is nice.Thanks!"
+
+letter = "Dear Nahid,\n\tThis python course is nice.\nThanks!"
+print(letter)
