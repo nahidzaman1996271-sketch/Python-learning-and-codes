1e953ce1ee41944583661ff5ce2a16ca7bb63260
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-05-06 00:30:37 +0600

    learned list

---
+++ Python codes/Chapter-04/02_list_methods.py
@@ -0,0 +1,10 @@
+# friends = ["Apple","Orange",5,345.05,"Nahid","Symoon"]
+# print(friends)
+# friends.append("Mahmuda")
+# print(friends)
+#l1.sort()
+# l1.reverse()
+# l1.insert(2,100) #insert 100 at index 2
+l1 = [4,2,7,2,8,9,1]
+value = l1.pop(3)
+print(value)
