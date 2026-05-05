0d3cecb3c16a38d922480c6eb7db0d67c36cc071
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-05-06 00:40:04 +0600

    learned tuple

---
+++ Python codes/Chapter-04/03_tuple.py
@@ -0,0 +1,3 @@
+a = (1,2,5,6, False, "Nahid", 3.14)
+print(a)
+print(type(a))
---
+++ Python codes/Chapter-04/04_tuple_methods.py
@@ -0,0 +1,8 @@
+a = (1,2,5,6, False, "Nahid", 3.14)
+print(a)
+print(type(a))
+
+no = a.count(45)
+print(no)
+i = a.index("Nahid")
+print(i)
