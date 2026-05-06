bf4d7036108b9da154781a739b06fb4b4cdd1133
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-05-06 12:02:00 +0600

    Starting the morning routine

---
+++ Python codes/Chapter-04-PS/03_problem3.py
@@ -0,0 +1,5 @@
+# Check that tuple type cannot be changed in Python
+
+a = (1,4,"Nahid")
+ 
+a[2] = "Ifti" # This will not work because tuple is immutable in Python. We cannot change the value of a tuple after it has been created.
