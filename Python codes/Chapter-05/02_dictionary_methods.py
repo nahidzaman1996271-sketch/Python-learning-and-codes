b2bf1a754f76c2b387449f8f3c99ba83c914812d
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-05-06 12:46:11 +0600

    just leant some dictionary methods there are other some methods but you guys can check it later

---
+++ Python codes/Chapter-05/02_dictionary_methods.py
@@ -0,0 +1,12 @@
+marks = {
+    "Nahid": 90,
+    "Mahmuda": 100,
+    "Mashuk": 90,
+    0: "Pranjol"
+}
+# print(marks.keys())
+# print(marks.items())
+# print(marks.values())
+# marks.update({"Nahid": 98,"Renuka": 88})
+# print(marks)
+print(marks.get("Nahid"))
