b3629ed11204b26ce40153be4455b8b5b2631ff9
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-05-07 20:10:23 +0600

    started learning again

---
+++ Python codes/Chapter-05-PS/06_problem6.py
@@ -0,0 +1,25 @@
+# Create an empty dictionary. Allow 4 friends to enter their favorite language as value and use key as their names. Assume the names are unique
+
+d = {}
+
+name = input("Enter friends name: ")
+lang = input("Enter Language name: ")
+d.update({name : lang})
+
+name = input("Enter friends name: ")
+lang = input("Enter Language name: ")
+d.update({name : lang})
+
+name = input("Enter friends name: ")
+lang = input("Enter Language name: ")
+d.update({name : lang})
+
+name = input("Enter friends name: ")
+lang = input("Enter Language name: ")
+d.update({name : lang})
+
+name = input("Enter friends name: ")
+lang = input("Enter Language name: ")
+d.update({name : lang})
+
+print(d)
---
+++ Python codes/Chapter-05-PS/07_problem7.py
@@ -0,0 +1,2 @@
+# Can you change the values inside a list which is contained in set S?
+# The values enter later will be updated
