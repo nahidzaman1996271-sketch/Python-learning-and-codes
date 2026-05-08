5e17796ecd98dfcc3805d09f8b78061ef8514946
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-05-08 16:42:40 +0600

    just started learning today's part

--- Python codes/Chapter-06/02_if_elif_else_ladder.py
+++ Python codes/Chapter-06/02_if_elif_else_ladder.py
@@ -1,11 +1,16 @@
-a = int(input("Enter your age: "))
+# If elif else ladder
 
-# If else ladder
+a = int(input("Enter your age: "))
 
 if(a>=18):
     print("You are above the age of consent")
     print("Good for you..")
 
+elif(a==0):
+    print("Zero is a invalid age, Retype again.")
+
+elif(a<0):
+    print("Negative age number is invalid, retype again.")
 
 else:
     print("You are below the age of consent")    
