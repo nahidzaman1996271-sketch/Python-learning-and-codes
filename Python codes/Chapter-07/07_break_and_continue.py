7bf557f4c88ba383649e5d39a4fe4a07cebf446b
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-05-10 22:58:22 +0600

    just learnt what is break and continue

--- Python codes/Chapter-07/07_break_and_continue.py
+++ Python codes/Chapter-07/07_break_and_continue.py
@@ -2,3 +2,9 @@ for i in range(100):
     if(i==34):
         break # Exit the loop immediately
     print(i)
+
+for i in range(100):
+    if(i==34):
+        continue # Skip this iteration
+    print(i)
+
