946921b71ead47b6d95f5ac787ddf124d0030e8a
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-05-10 22:45:24 +0600

    just learnt another loops

---
+++ Python codes/Chapter-07/05_for_loop_iterate.py
@@ -0,0 +1,14 @@
+## For loop with lists
+l = [1,2,4,5,6,7]
+for i in l:
+    print(i)
+
+## For loops with Tuples
+t  = (1,2,4,5,6,7,8)
+for i in t:
+    print(i) 
+    
+## For loop with strings
+m = "Nahid"
+for i in m:
+    print(i)
