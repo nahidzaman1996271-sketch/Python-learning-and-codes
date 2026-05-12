4bb6c6eddff9cd849f54a4d3902ef408de8abfce
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-05-12 17:37:45 +0600

    just learnt a bunch of things

--- Python codes/Chapter-08/01_functions.py
+++ Python codes/Chapter-08/01_functions.py
@@ -1,12 +1,15 @@
-# a = int(input("Enter the number: "))
+'''
+a = int(input("Enter the number: "))
 
-# b = int(input("Enter the number: "))
+b = int(input("Enter the number: "))
 
-# c = int(input("Enter the number: "))
+c = int(input("Enter the number: "))
 
-# average = (a + b + c)/3
-# print("The average: ", average)
+average = (a + b + c)/3
+print("The average: ", average)
+'''
 
+# Function Definition
 def avg():
     a = int(input("Enter the number: "))
     b = int(input("Enter the number: "))
@@ -15,9 +18,14 @@ def avg():
     average = (a + b + c)/3
     print("The average: ", average)
 
-
+# Function Call
 avg()
+print("Thank you!")
 avg()
+print("Thank you!")
 avg()
+print("Thank you!")
 avg()
+print("Thank you!")
 avg()
+print("Thank you!")
