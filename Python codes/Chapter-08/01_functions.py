b1f395d93c2442ac503c878a4ef9926efd468e6d
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-05-12 17:31:44 +0600

    started learning function and it is easy to me

---
+++ Python codes/Chapter-08/01_functions.py
@@ -0,0 +1,23 @@
+# a = int(input("Enter the number: "))
+
+# b = int(input("Enter the number: "))
+
+# c = int(input("Enter the number: "))
+
+# average = (a + b + c)/3
+# print("The average: ", average)
+
+def avg():
+    a = int(input("Enter the number: "))
+    b = int(input("Enter the number: "))
+    c = int(input("Enter the number: "))
+
+    average = (a + b + c)/3
+    print("The average: ", average)
+
+
+avg()
+avg()
+avg()
+avg()
+avg()
