39ecf4065933b1aae314f44a3e1d7b857316d54c
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-05-14 23:43:10 +0600

    we learnt so many Python codes and info today and this is it for today see ya guys tomorrow take care..

--- Python codes/Chapter-10/03_self.py
+++ Python codes/Chapter-10/03_self.py
@@ -1,8 +1,11 @@
 class Employee:
     language = "Python" # This is a class attribute
     salary = 120000
-
+    
+    def getInfo(self):
+        print(f"The language is {self.language}. The salary is {self.salary}")
 
 nahid = Employee()
 nahid.language = "Javascript" # This is an instance attribute
-print(nahid.language, nahid.salary)
+# harry.getInfo()
+Employee.getInfo(nahid)
