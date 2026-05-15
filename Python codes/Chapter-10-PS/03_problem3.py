e0f21a48c33198ba9a7a6cfe722c6baf8afb60a7
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-05-15 21:53:38 +0600

    haha this one is crazy and i love it

---
+++ Python codes/Chapter-10-PS/03_problem3.py
@@ -0,0 +1,14 @@
+'''
+Create a class with a class attribute a; create an object from it and set 'a'
+directly using object.a = o. Does this change the class attribute?
+'''
+class Demo:
+    a = 4
+
+o = Demo()
+print(o.a) # Prints the class attribute because instance attribute is not present
+o.a = 0 # Instance attribute is set
+print(o.a) # Prints the instance attribute because instance attribute is set
+print(Demo.a) # Prints the class attribute
+
+# And the final answer is no it doesn't change the class attribute
