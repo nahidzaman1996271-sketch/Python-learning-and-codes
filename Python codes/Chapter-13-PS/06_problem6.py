71cb333cf15c447af768717aa7d08ed8b0e9df48
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-05-22 22:16:48 +0600

    just modified few things in the program

--- Python codes/Chapter-13-PS/06_problem6.py
+++ Python codes/Chapter-13-PS/06_problem6.py
@@ -1,3 +1,6 @@
 '''
 Run pip freeze for the system interpreter. Take the contents and create a similar virtualenv.
 '''
+
+# pip freeze > requirements.txt
+# virtualenv Nahid
---
+++ Python codes/Chapter-13-PS/07_problem7.py
@@ -0,0 +1,3 @@
+'''
+Explore the 'Flask' module and create a web server using Flask and Python..
+'''
