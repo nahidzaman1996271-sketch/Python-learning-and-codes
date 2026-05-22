7b22bfb9607413c4773b8d8887cc231098f60a20
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-05-22 10:05:14 +0600

    now started learning global

--- Python codes/Chapter-12/module.py
+++ Python codes/Chapter-12/module.py
@@ -1,5 +1,9 @@
 def myFunc():
     print("Hello World!")
 
-myFunc()    
+if __name__ == "__main__":
+   #if this code is executed by running the file its present in
+   print("We are directly running this code")
+
+   myFunc()    
 print(__name__)
