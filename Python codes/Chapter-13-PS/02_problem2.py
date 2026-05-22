a646c10c558acadfce6ef7189be5d0d718085968
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-05-22 21:48:30 +0600

    now i solved problem number two and it is helps me a lot to understand the format fucntion

---
+++ Python codes/Chapter-13-PS/02_problem2.py
@@ -0,0 +1,13 @@
+'''
+Write a program to input name, marks and phone number of a student and format
+it using the format function like below
+
+"The name of the student is Harrt, his makes are 72 and phone number is 9999888"
+'''
+
+name = input("Enter name: ")
+marks = int(input("Enter marks: "))
+phone = int(input("Phone number: "))
+
+s = "The name of the student is {}, his makes are {} and phone number is {}".format(name,marks,phone)
+print(s)
