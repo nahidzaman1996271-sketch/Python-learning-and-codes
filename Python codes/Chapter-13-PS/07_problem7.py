17ac55e90ee915f17e02d22707ca327dbed945a5
Nahid Ibn Zaman <nahidzaman1996271@gmail.com>
2026-05-22 22:23:22 +0600

    and here end's the chapter 13 practice set

--- Python codes/Chapter-13-PS/07_problem7.py
+++ Python codes/Chapter-13-PS/07_problem7.py
@@ -1,3 +1,12 @@
 '''
 Explore the 'Flask' module and create a web server using Flask and Python..
 '''
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World this is me Nahid!</p>"
+
+app.run()
